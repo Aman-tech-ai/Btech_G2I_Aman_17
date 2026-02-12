2a74ab73eb1dd6846ab2bfc16947f72c68b322d8
Aman-tech-ai <25121cn141@glbitm.ac.in>
2026-02-12 11:07:25 +0530

    Add files via upload
    To find if the two given numbers are equal or not.

---
+++ new.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a==b){
+        printf("no. are equal.");
+    }
+    else{printf(" not equal.");
+    }
+    return 0;
+}
